f50e02205bbc19b18d0882456fdec91caaf77c39
JViveka <anushankar1526@gmail.com>
2021-11-14 20:18:16 +0530

    Arrays

---
+++ src/arrayList/Collections.java
@@ -0,0 +1,22 @@
+package arrayList;
+
+public class Collections {
+
+	public static void main(String[] args) {
+		int num[]=new int[5];
+		num[0]=10;
+		num[1]=10;
+		num[2]=10;
+		num[3]=10;
+		num[4]=10;
+		num[3]=500;
+		//System.out.println(num[3]);
+		//to find array lenght
+		//int len= num.length;
+		//System.out.println(len);
+		// to used for loop
+		for(int j=0; j<num.length;j++);
+		//System.out.println(num[]);
+	}
+
+}
